24790b8862a30f6d3b41ea3895ae4d1fd481f4ce
Kevin Bongart <contact@kevinbongart.net>
2018-05-29 19:02:45 -0400

    middleman-s3_sync and gzip

--- Gemfile
+++ Gemfile
@@ -1,10 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'dotenv'
 gem 'middleman'
 gem 'middleman-autoprefixer'
 gem 'middleman-imageoptim', git: 'https://github.com/plasticine/middleman-imageoptim'
 gem 'middleman-minify-html'
-
-gem 'aws-sdk'
-gem 'dotenv'
-gem 'rake'
+gem 'middleman-s3_sync'
--- config.rb
+++ config.rb
@@ -7,15 +7,18 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-configure :build do
-  activate :minify_javascript
-end
-
 activate :external_pipeline,
   name: :gulp,
   command: 'gulp',
   source: 'tmp/build'
 
-activate :gzip
-activate :imageoptim
-activate :minify_html
+configure :build do
+  activate :gzip
+  activate :imageoptim
+  activate :minify_html
+  activate :minify_javascript
+end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.region = 'us-east-1'
+end
