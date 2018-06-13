8a2a8ff092c95e4d6403e8864d18bee979d84bac
Kevin Bongart <contact@kevinbongart.net>
2018-06-13 09:46:09 -0400

    Update npm

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 
-gulp.task('css', function(){
+gulp.task('css', function() {
   return gulp.src(['source/stylesheets/vendor/*.css', 'source/stylesheets/*.css'])
     .pipe(minifyCSS())
     .pipe(concat('site.css'))
